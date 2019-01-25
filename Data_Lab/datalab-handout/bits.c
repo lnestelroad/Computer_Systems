634ef77b5c3084219dd3cb2ce736608b57a9ea7f
Liam Nestelroad <nestelroadliam@gmail.com>
2019-01-24 19:38:38 -0700

    test for bits.c

--- Data_Lab/datalab-handout/bits.c
+++ Data_Lab/datalab-handout/bits.c
@@ -244,18 +244,15 @@ int anyOddBit(int x) {
  */
 int byteSwap(int x, int n, int m) {
     int mask = 0xff;
+    int nshift = n << 3;
+    int mshift = m << 3;
 
-    int tmp = (m + ~n + 1) << 3;
+    int firstByte = x >> nshift;
+    int secondByte = x >> mshift;
 
-    int firstByte = (x & (mask << (n << 3))) << (tmp);
-    int SecondByte = (x & (mask << (m << 3))) >> (tmp);
-    
-    int maskShift = (mask << (n << 3));
-    int maskShift2 = (mask << (m << 3));
-    int leftover = x & (~(maskShift | maskShift2));
-    /*int maskShift = x & (~(mask << (n << 3)) | ~(mask << (m << 3))); zeros out the byte positions to be swapped*/
-    int comeTogether = firstByte | SecondByte | leftover;
-    return comeTogether;
+    int leftover = ~(mask << nshift) & ~(mask << mshift);
+
+    return (x & leftover) + (firstByte << mshift) + (secondByte << nshift);
 }
 /* 
  * addOK - Determine if can compute x+y without overflow
