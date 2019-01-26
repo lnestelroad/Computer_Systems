f3e836c79153785b47308e03330fc0eec60b785b
Liam Nestelroad <nestelroadliam@gmail.com>
2019-01-25 19:28:15 -0700

    test for bits.c

--- Data_Lab/datalab-handout/bits.c
+++ Data_Lab/datalab-handout/bits.c
@@ -363,7 +363,7 @@ int satAdd(int x, int y) {
   int overflow = ((~mask) & lowest) | ((mask) & highest);
 
   int mask2 = (!check) + ~0x00;
-  return ((~mask2) & add) | ((mask) & overflow); 
+  return ((~mask2) & add) | ((mask2) & overflow); 
 }
 /*
  * Extra credit
